4be793bcec68d3abf87bb68eadab876427f7e246
Brad McAlister <github@sonicparke.com>
2013-10-23 15:10:13 -0500

    fixing line endings

--- src/worker_loader.js
+++ src/worker_loader.js
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- /* globals PDFJS, Util */
+ /* globals PDFJS, Util **/
 
 'use strict';
 
